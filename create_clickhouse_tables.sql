4686b13ceedbe6804a6a4c2cd1f228ee6dd87444
TPShipilova <138506392+TPShipilova@users.noreply.github.com>
2025-05-30 16:03:32 +0300

    Update create_clickhouse_tables.sql

--- create_clickhouse_tables.sql
+++ create_clickhouse_tables.sql
@@ -1,4 +1,3 @@
--- 1. Витрина продаж по продуктам
 CREATE TABLE IF NOT EXISTS product_sales_dashboard (
     product_id Int32,
     product_name String,
@@ -10,7 +9,6 @@ CREATE TABLE IF NOT EXISTS product_sales_dashboard (
     etl_time DateTime DEFAULT now()
 ) ENGINE = Log();
 
--- 2. Топ продуктов
 CREATE TABLE IF NOT EXISTS top_products (
     product_id Int32,
     product_name String,
@@ -22,7 +20,6 @@ CREATE TABLE IF NOT EXISTS top_products (
     rank UInt32
 ) ENGINE = Log();
 
--- 3. Витрина продаж по клиентам
 CREATE TABLE IF NOT EXISTS customer_sales_dashboard (
     customer_id Int32,
     customer_first_name String,
@@ -33,7 +30,6 @@ CREATE TABLE IF NOT EXISTS customer_sales_dashboard (
     avg_order_value Float64
 ) ENGINE = Log();
 
--- 4. Топ клиентов
 CREATE TABLE IF NOT EXISTS top_customers (
     customer_id Int32,
     customer_first_name String,
@@ -45,7 +41,6 @@ CREATE TABLE IF NOT EXISTS top_customers (
     rank UInt32
 ) ENGINE = Log();
 
--- 5. Витрина продаж по времени
 CREATE TABLE IF NOT EXISTS time_sales_dashboard (
     year Int32,
     month Int32,
@@ -54,7 +49,6 @@ CREATE TABLE IF NOT EXISTS time_sales_dashboard (
     order_count Int32
 ) ENGINE = Log();
 
--- 6. Витрина продаж по магазинам
 CREATE TABLE IF NOT EXISTS store_sales_dashboard (
     store_id Int32,
     store_city String,
@@ -64,7 +58,6 @@ CREATE TABLE IF NOT EXISTS store_sales_dashboard (
     avg_order_value Float64
 ) ENGINE = Log();
 
--- 7. Топ магазинов
 CREATE TABLE IF NOT EXISTS top_stores (
     store_id Int32,
     store_city String,
@@ -75,7 +68,6 @@ CREATE TABLE IF NOT EXISTS top_stores (
     rank UInt32
 ) ENGINE = Log();
 
--- 8. Витрина продаж по поставщикам
 CREATE TABLE IF NOT EXISTS supplier_sales_dashboard (
     supplier_id Int32,
     supplier_country String,
@@ -84,7 +76,6 @@ CREATE TABLE IF NOT EXISTS supplier_sales_dashboard (
     sales_count Int32
 ) ENGINE = Log();
 
--- 9. Топ поставщиков
 CREATE TABLE IF NOT EXISTS top_suppliers (
     supplier_id Int32,
     supplier_country String,
@@ -94,7 +85,6 @@ CREATE TABLE IF NOT EXISTS top_suppliers (
     rank UInt32
 ) ENGINE = Log();
 
--- 10. Витрина качества продукции
 CREATE TABLE IF NOT EXISTS product_quality_dashboard (
     product_id Int32,
     product_name String,
